8955bc48de59dfb0b1e93d7679ea53818c93d19d
Ryan Livengood <149983171+Ryan-Livengood@users.noreply.github.com>
2023-11-24 11:43:41 -0800

    removed forked repos, added 4H website, removed personal projects

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -6,7 +6,7 @@ const config = {
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: true, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
@@ -37,7 +37,7 @@ const config = {
       position: 'Software Engineering Intern',
       from: 'September 2023',
       to: 'Present',
-      companyLink: '',
+      companyLink: 'https://www.4thhurdle.com',
     },
     {
       company: 'Nordstrom',
@@ -67,9 +67,9 @@ const config = {
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: '',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+        '',
       imageUrl: 'https://via.placeholder.com/250x250',
       link: 'https://example.com',
     },
